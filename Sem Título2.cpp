81e7ea6db97f4aeb646778af1f80d23615ee699a
CarlosEduCampos <zumbidbz@gmail.com>
2020-09-10 23:13:54 -0300

    cadastro
    add de menu para auxiliar o usuario

--- Sem Título2.cpp
+++ Sem Título2.cpp
@@ -6,12 +6,23 @@ int main()
 	string nome, part = ""; // variaveis nome do eleitor e partirdo
 	int voto = 0; // variavel recebe numero do candidato
 	
+	// Cadastro 
 	cout << " Nome: ";
 	cin >> nome;
 	linha
 	cout << " Partido: ";
 	cin >> part;
 	linha
-	cout << "Candidato:{voto valido 1 ao 5} ";
+	linha
+	cout << " 1 ate 5 - Votos validos para candidatos.\n";
+	cout << "\n";
+	cout << " 6 - Voto valido como indeciso.\n";
+	cout << "\n";
+	cout << " 7 - Voto valido como nulo ou branco.\n";
+	linha
+	linha 
+	cout << " Candidato: ";
 	cin >> voto;
+	linha
+	
 }
