a93f35181169f236450ab1048e4b1f53661f40ff
CarlosEduCampos <zumbidbz@gmail.com>
2020-10-10 12:24:08 -0300

    add prova atualização binario

--- Sem Título1.cpp
+++ Sem Título1.cpp
@@ -5,27 +5,25 @@ using namespace std;
 //
 int main(){
 //
-		char bin[8];//binario.
+		char bin[20];//binario.
 		int dec = 0, i;// decimal , contador.
 		//
 		cout <<"\n Binario: ";//Pedindo binario.
 		cin >> bin;//Recebendo binario.
 		//
-		int num,mul; // inteior recebe 0 ou 1, multiplicação de num por 2.
+		int c = 0; // contador.
 		//
-		for(i=strlen(bin)-1; i>=0; i--)// lupe  multiplica variavel por 2 eleva pela posição e soma em decimal.
-		{
-			if(bin[i]== "0")// transformando str em int.			
-				num = 0; 
+		for( i=(strlen(bin)-1); i>=0; i--)// numero 0 ou 1 elevado pela posição.
+		{	
+			if(bin[c] == '0'){ // transformando str para int.
+				dec += 0;
 			}
-			else if(bin[i] == "1")// transformando str em int.
-			{
-				num = 1;
+			else if(bin[c] == '1'){ // transformando str para int.
+				dec += pow(2,(i));
 			}
-			mul = num *2;// multiplicação por 2
-			dec += pow(mul,i);// somando potenciação em decimal.
+			c++;
 		}
 		//
-		cout <<" DECIMAL : "<<dec<<"\n";// mostrando resultado.
+		cout <<"\n DECIMAL : "<<dec<<"\n";// mostrando resultado.
 	return 0;
 }
